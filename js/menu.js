c8cfa421d45b8bf270acf9bd06aaf3f875d771b6
nkfld <93005829+nkfld@users.noreply.github.com>
2021-11-08 19:47:43 +0300

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,63 @@
-const renderItems = (data) => {
-     console.log(data);
+const cardsMenu = document.querySelector('.cards-menu');
+
+const changeTitle = (restaraunt) => {
+    const restaurantTitle = document.querySelector('.restaurant-title') 
+    const rating = document.querySelector('.rating')
+    const price = document.querySelector('.price')
+    const category = document.querySelector('.category')
+    category.textContent = restaraunt.kitchen
+    price.textContent = (`ОТ ${restaraunt.price} ₽`)
+    rating.textContent = restaraunt.stars
+    restaurantTitle.textContent = restaraunt.name
 }
 
 
-fetch (`./db/partners.json`)
-.then((response) => response.json()) 
-.then ((data) => {
-    renderItems(data)
-})
-.catch ((error) => {
-    console.log(error);
-})
+
+const renderItems = (data) => {
+     data.forEach(({description, id, image, name , price}) => {
+        const div = document.createElement ('div');
+        div.classList.add ('card')
+        
+        div.innerHTML = `<img src="${image}" alt="image" class="card-image" />
+        <div class="card-text">
+            <div class="card-heading">
+                <h3 class="card-title card-title-reg">${name}</h3>
+            </div>
+            <div class="card-info">
+                <div class="ingredients">${description}
+                </div>
+            </div>
+            <div class="card-buttons">
+                <button class="button button-primary button-add-cart">
+                    <span class="button-card-text">В корзину</span>
+                    <span class="button-cart-svg"></span>
+                </button>
+                <strong class="card-price-bold">${price}₽</strong>
+            </div>
+        </div>`
+
+        cardsMenu.append(div);
+     });
+}
+
+if (localStorage.getItem('restaraunts')) {
+
+    const restaraunt = JSON.parse(localStorage.getItem('restaraunts'))
+
+    changeTitle(restaraunt)
+
+    fetch (`./db/${restaraunt.products}`)
+    .then((response) => response.json()) 
+    .then ((data) => {
+        renderItems(data)
+    })
+    .catch ((error) => {
+        console.log(error);
+    }) 
+}
+else {
+    window.location.href = '/index.html'
+}
+
+
 
